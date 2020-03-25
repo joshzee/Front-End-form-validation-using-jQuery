60c7b7e5e4e3e929e3ff5fb60f30fa8b2c5cfee0
joshzee <joshuazarazovski@gmail.com>
2020-03-25 20:32:06 +1100

    fixed confirm password issue

--- external.js
+++ external.js
@@ -21,7 +21,7 @@ $(function() {
 			required: true,
 			minlength: 5
 		},
-		confirmpassword: {
+		password__confirm: {
 			equalTo: "#password"
 		}
 		},
@@ -37,7 +37,7 @@ $(function() {
 			required: "Please provide a password",
 			minlength: "Your password must be at least 5 characters long"
 		},		
-		confirmpassword: "Password does not match"
+		password__confirm: "Password does not match"
 		},
 		// Make sure the form is submitted to the destination defined
 		// in the "action" attribute of the form when valid
